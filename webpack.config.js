6d031076566e22c805984cb08882b3e94b21ddb6
Gabriel Arndt <gabrielharndt@hotmail.com>
2024-06-14 12:55:10 -0300

    up

--- webpack.config.js
+++ webpack.config.js
@@ -1,94 +1,92 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
-
 module.exports = {
-    mode: 'development',
-
-    entry: './src/index.js',
-    output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                },
-                parser: {
-                    commonjsMagicComments: true
-                }
-            },
-            {
-                test: /\.cs$/,
-                use: 'null-loader'
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.html$/,
-                use: ['html-loader']
-            },
-        ]
-    },
-    resolve: {
-        modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-        fallback: {
-            "fs": false,
-            "net": false,
-            "tls": false,
-            "crypto": require.resolve("crypto-browserify"),
-            "http": require.resolve("stream-http"),
-            "https": require.resolve("https-browserify"),
-            "stream": require.resolve("stream-browserify"),
-            "path": require.resolve("path-browserify"),
-            "dgram": false,
-            "child_process": false,
-            "timers": require.resolve("timers-browserify"),
-            "zlib": require.resolve("browserify-zlib"),
-            "os": require.resolve("os-browserify/browser"),
-            "assert": require.resolve("assert"),
-            "util": require.resolve("util"),
-            "url": require.resolve("url"),
-            "vm": require.resolve("vm-browserify"),
-            "constants": require.resolve("constants-browserify"),
-            "async_hooks": false,
-            "timers/promises": false
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js', // Use placeholders like [name], [id], etc., for dynamic naming
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
-        alias: {
-            timers: path.resolve(__dirname, 'node_modules/timers-browserify'),
-            'timers/promises': path.resolve(__dirname, 'node_modules/timers-browserify/main.js')
-        }
+      },
+      {
+        test: /\.cs$/,
+        use: 'null-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+    ],
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    fallback: {
+      fs: false,
+      net: false,
+      tls: false,
+      crypto: require.resolve('crypto-browserify'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      stream: require.resolve('stream-browserify'),
+      path: require.resolve('path-browserify'),
+      dgram: false,
+      child_process: false,
+      timers: require.resolve('timers-browserify'),
+      zlib: require.resolve('browserify-zlib'),
+      os: require.resolve('os-browserify/browser'),
+      assert: require.resolve('assert'),
+      util: require.resolve('util'),
+      url: require.resolve('url'),
+      vm: require.resolve('vm-browserify'),
+      constants: require.resolve('constants-browserify'),
+      async_hooks: false,
+      'timers/promises': false,
     },
-    devtool: 'source-map', // Adiciona mapas de fonte para melhor depuração
-    devServer: {
-        static: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9000
+    alias: {
+      timers: path.resolve(__dirname, 'node_modules/timers-browserify'),
+      'timers/promises': path.resolve(
+        __dirname,
+        'node_modules/timers-browserify/main.js'
+      ),
     },
-    performance: {
-        hints: false // Desativa dicas de performance
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+  performance: {
+    hints: false,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
     },
-    optimization: {
-        runtimeChunk: 'single',
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all'
-                }
-            }
-        }
-    },
-    externals: [nodeExternals()],
+  },
+  externals: [nodeExternals()],
 };
